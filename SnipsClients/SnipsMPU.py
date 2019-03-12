568633088ae4fc03b19de1d234c034ff14565ba6
CoorFun <coorfang@gmail.com>
2019-03-12 18:54:30 +0100

    add site id checking

--- SnipsClients/SnipsMPU.py
+++ SnipsClients/SnipsMPU.py
@@ -7,27 +7,31 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 
 class SnipsMPU(object):
-    def __init__(self, mqtt_addr, relay, sht31):
+    def __init__(self, mqtt_addr, site_id, relay, sht31):
         self.THRESHOLD_INTENT_CONFCORE_DROP = 0.3
         self.THRESHOLD_INTENT_CONFCORE_TAKE = 0.6
         self.THRESHOLD_SLTO_CONFCORE_TAKE = 0.5
 
+        self.__sitd_id = site_id
         self.__relay = relay
         self.__sht31 = sht31
 
         self.__mqtt_addr = mqtt_addr
 
-    def __extract_value(self, slots):
-        result = []
-        if slots is not None:
-            for slot in slots:
-                if slot.confidenceScore > self.__THRESHOLD_SLTO_CONFCORE_TAKE:
-                    result.append(slot.slot_value.value.value)
-        return result
+    def check_site_id(handler):
+        @functools.wraps(handler)
+        def wrapper(self, hermes, intent_message):
+            if intent_message.site_id != self.__sitd_id:
+                return None
+            else:
+                return handler(self, hermes, intent_message)
+        return wrapper
 
     def check_confidence_score(handler):
         @functools.wraps(handler)
         def wrapper(self, hermes, intent_message):
+            if handler is None:
+                return None
             if intent_message.intent.confidence_score < self.THRESHOLD_INTENT_CONFCORE_DROP:
                 hermes.publish_end_session(intent_message.session_id, '')
                 return None
@@ -35,28 +39,31 @@ class SnipsMPU(object):
                 hermes.publish_end_session(intent_message.session_id, 'Sorry, I dont think I understood')
                 return None
             return handler(self, hermes, intent_message)
-
         return wrapper
 
     @check_confidence_score
+    @check_site_id
     def handler_relay_turn_on(self, hermes, intent_message):
         print("Relay Turn On")
         self.__relay.turn_on()
         hermes.publish_end_session(intent_message.session_id, 'Relay has been turned on')
 
     @check_confidence_score
+    @check_site_id
     def handler_relay_turn_off(self, hermes, intent_message):
         print("Relay Turn Off")
         self.__relay.turn_off()
         hermes.publish_end_session(intent_message.session_id, 'Relay has been turned off')
 
     @check_confidence_score
+    @check_site_id
     def handler_check_humidity(self, hermes, intent_message):
         print("Humidity Check")
         humidity = self.__sht31.get_humidity_string()
         hermes.publish_end_session(intent_message.session_id, 'The current humidity is {}'.format(humidity))
 
     @check_confidence_score
+    @check_site_id
     def handler_check_temperature(self, hermes, intent_message):
         print("Temperature Check")
         temperature = self.__sht31.get_temperature_string()
--- action-demo_dev_kit.py
+++ action-demo_dev_kit.py
@@ -13,14 +13,13 @@ config = SnipsConfigParser.read_configuration_file(CONFIG_INI).get('global')
 MQTT_ADDR_HOST = str(config.get('mqtt_host'))
 MQTT_ADDR_PORT = str(config.get('mqtt_port'))
 MQTT_ADDR = "{}:{}".format(MQTT_ADDR_HOST, MQTT_ADDR_PORT)
-
-RELAY_GPIO = int(config.get('relay_gpio'))
+SITE_ID = str(config.get('site_id'))
+RELAY_GPIO = int(config.get('relay_gpio_bcm'))
 TEMP_UNIT = str(config.get('temperature_unit'))
 
 relay = RelaySwitch.RelaySwitch('screen', RELAY_GPIO)
 sht31 = SHT31.SHT31(TEMP_UNIT)
-
-client = SnipsMPU.SnipsMPU(MQTT_ADDR, relay, sht31)
+client = SnipsMPU.SnipsMPU(MQTT_ADDR, SITE_ID, relay, sht31)
 
 if __name__ == "__main__":
     try:
