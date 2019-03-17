b7d824f678b4b086a680aef9094916011dbce5e6
CoorFun <coorfang@gmail.com>
2019-03-17 14:28:44 +0100

    SnipsI18n translation generator

--- snipsTools.py
+++ snipsTools.py
@@ -1,16 +1,22 @@
 import configparser
 import io
+import json
+import re
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
+ENCODING_FORMAT = "utf-8"
 
 class SnipsConfigParser(configparser.ConfigParser):
     def to_dict(self):
-        return {section: {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+        return {
+            section: {
+                option_name : option for option_name, option in self.items(section)
+            } for section in self.sections()
+        }
 
     @staticmethod
     def read_configuration_file(configuration_file):
         try:
-            with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            with io.open(configuration_file, encoding=ENCODING_FORMAT) as f:
                 conf_parser = SnipsConfigParser()
                 conf_parser.readfp(f)
                 return conf_parser.to_dict()
@@ -32,3 +38,36 @@ class SnipsConfigParser(configparser.ConfigParser):
             print(e)
             return False
 
+class SnipsI18n(object):
+    __dic = {}
+
+    __path = None
+    __locale = None
+
+    def __init__(self, path, locale = 'en_US'):
+        self.__path = path
+        self.__locale = locale
+        self.__load_dictionary()
+
+    def __load_dictionary(self):
+        dir = '{}/{}.json'.format(self.__path, self.__locale)
+        try:
+            with open(dir, 'r', encoding=ENCODING_FORMAT) as f:
+                self.__dic = json.loads(f.read())
+        except IOError as e:
+            print (e)
+
+    def get(self, raw_key, parameters = {}):
+        keys = raw_key.split('.')
+        temp = self.__dic
+
+        for key in keys:
+            temp = temp.get(key, 'null')
+
+        if not parameters or temp == 'null':
+            return temp
+        else:
+            for key in parameters:
+                pattern = '(\{){2}(\s)*(' + key +'){1}(\s)*(\}){2}'
+                temp = re.sub(pattern, str(parameters[key]), temp)
+            return temp
